f94b7fa38951a5dbfb604a0e47baa11ec994e9bd
BradenEverson <bradeneverson@gmail.com>
2025-06-29 08:43:00 -0500

    Add GPIO pins

--- src/main.zig
+++ src/main.zig
@@ -14,18 +14,19 @@ export fn stepper_task(params: ?*anyopaque) callconv(.c) void {
     _ = params;
 
     while (true) {
-        c.HAL_GPIO_WritePin(c.GPIOA, c.GPIO_PIN_2, c.GPIO_PIN_SET);
+        c.HAL_GPIO_TogglePin(c.LD2_GPIO_Port, c.LD2_Pin);
+        c.HAL_GPIO_WritePin(c.GPIOC, c.GPIO_PIN_9, c.GPIO_PIN_SET);
 
-        c.HAL_GPIO_WritePin(c.GPIOA, c.GPIO_PIN_3, c.GPIO_PIN_SET);
+        c.HAL_GPIO_WritePin(c.GPIOC, c.GPIO_PIN_8, c.GPIO_PIN_SET);
         os.vTaskDelay(1);
-        c.HAL_GPIO_WritePin(c.GPIOA, c.GPIO_PIN_3, c.GPIO_PIN_RESET);
+        c.HAL_GPIO_WritePin(c.GPIOC, c.GPIO_PIN_8, c.GPIO_PIN_RESET);
         os.vTaskDelay(1);
     }
 }
 
 export fn entry() callconv(.c) void {
     var step_pin: c.GPIO_InitTypeDef = .{
-        .Pin = c.GPIO_PIN_3,
+        .Pin = c.GPIO_PIN_8,
         .Mode = c.GPIO_MODE_OUTPUT_PP,
         .Pull = c.GPIO_NOPULL,
         .Speed = c.GPIO_SPEED_FREQ_HIGH,
@@ -33,12 +34,12 @@ export fn entry() callconv(.c) void {
     c.HAL_GPIO_Init(c.GPIOA, &step_pin);
 
     var dir_pin: c.GPIO_InitTypeDef = .{
-        .Pin = c.GPIO_PIN_2,
+        .Pin = c.GPIO_PIN_9,
         .Mode = c.GPIO_MODE_OUTPUT_PP,
         .Pull = c.GPIO_NOPULL,
         .Speed = c.GPIO_SPEED_FREQ_HIGH,
     };
-    c.HAL_GPIO_Init(c.GPIOA, &dir_pin);
+    c.HAL_GPIO_Init(c.GPIOC, &dir_pin);
 
     // Create FreeRTOS task
     const pvParameters: ?*anyopaque = null;
