617badf78ecc777a333f9fbe6a8632c47fdd0810
BradenEverson <bradeneverson@gmail.com>
2025-07-01 04:40:26 -0500

    Update tokenizer

--- src/gcode/tokenizer.zig
+++ src/gcode/tokenizer.zig
@@ -2,6 +2,7 @@
 
 const std = @import("std");
 
+/// All types a GCODE token can have
 pub const TokenTag = enum(u16) {
     g_command,
     m_command,
@@ -27,10 +28,15 @@ pub const TokenTag = enum(u16) {
     function,
 };
 
+/// A GCODE Token - a tag, index into a buffer and length in that buffer
 pub const Token = struct {
     tag: TokenTag,
     idx: u32,
     len: u16,
 };
 
-pub fn tokenize() !void {}
+/// Attempts to tokenize a GCODE stream into a list of tokens, reporting any errors along the way
+pub fn tokenize(stream: []const u8, buf: *std.ArrayList(Token)) !void {
+    _ = stream;
+    _ = buf;
+}
