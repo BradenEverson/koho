bb31c36d00cd6c595439ac687f2b59165e7bddf5
BradenEverson <bradeneverson@gmail.com>
2025-07-02 03:35:48 -0500

    Create homing function

--- src/printer/printer.zig
+++ src/printer/printer.zig
@@ -2,6 +2,10 @@
 
 const Stepper = @import("stepper.zig");
 
+curr_x: u64,
+curr_y: u64,
+curr_z: u64,
+
 /// X-Axis Stepper Motor
 x: Stepper,
 /// Y-Axis Stepper Motor
@@ -15,11 +19,28 @@ feed: Stepper,
 
 const Self = @This();
 
+/// Creates a new printer system
 pub fn init(x: Stepper, y: Stepper, z: Stepper, feed: Stepper) Self {
-    Self{
+    return Self{
         .x = x,
         .y = y,
         .z = z,
         .feed = feed,
+
+        // We don't know where we are until we zero the axis
+        .curr_x = undefined,
+        .curr_y = undefined,
+        .curr_z = undefined,
     };
+}
+
+/// Homes all axis
+pub fn zero(self: *Self) void {
+    // TODO: Actually move the steppers until they all hit
+    // endstops, causing an interrupt that should set some
+    // flag or something
+
+    self.curr_x = 0;
+    self.curr_y = 0;
+    self.curr_z = 0;
 }
