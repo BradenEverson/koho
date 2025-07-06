5c288b029f87eb7ebd67ca94f86dafd9850530b7
BradenEverson <bradeneverson@gmail.com>
2025-07-06 16:00:22 -0500

    Create parser

--- src/gcode/executor.zig
+++ src/gcode/executor.zig
@@ -1,1 +1,19 @@
 //! GCODE AST Executor
+
+const ptable = @import("../printer/vtable.zig");
+const Expr = @import("parser.zig").Expr;
+
+vtable: ptable,
+
+const Self = @This();
+
+pub fn init(vtable: ptable) Self {
+    return Self{
+        .vtable = vtable,
+    };
+}
+
+pub fn exec(self: *Self, gcode: []Expr) !void {
+    _ = self;
+    _ = gcode;
+}
--- src/gcode/parser.zig
+++ src/gcode/parser.zig
@@ -1,1 +1,17 @@
 //! GCODE Parser - Converts flat token stream to AST
+
+const Token = @import("tokenizer.zig").Token;
+
+pub const Expr = union(enum) {};
+
+pub const Parser = struct {
+    stream: []Token,
+    cursor: u32,
+
+    pub fn init(tokens: []Token) Parser {
+        return Parser{
+            .stream = tokens,
+            .cursor = 0,
+        };
+    }
+};
