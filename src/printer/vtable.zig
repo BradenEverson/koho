e63d24796cd48f2d5b254f99855a91ada5732243
BradenEverson <bradeneverson@gmail.com>
2025-07-06 15:44:54 -0500

    Forgot I can make the vtable just a file struct

--- src/printer/vtable.zig
+++ src/printer/vtable.zig
@@ -1,17 +1,14 @@
-//! Printer's vtable
+//! The VTable of functions for any printer, making our whole shebang modular
 
-/// The VTable of functions for any printer, making our whole shebang modular
-pub const PrinterVtable = struct {
-    /// Move on the X Axis
-    move_x: *const fn (dir: u8, steps: u32) void,
-    /// Move on the Y Axis
-    move_y: *const fn (dir: u8, steps: u32) void,
-    /// Move on the Z Axis
-    move_z: *const fn (dir: u8, steps: u32) void,
-    /// Move the nozzle extruder
-    move_extruder: *const fn (steps: u32) void,
-    /// Set the bed's temperature
-    set_bed_temp: *const fn (deg_f: f32) void,
-    /// Set the temperature of the nozzle
-    set_nozzle_temp: *const fn (deg_f: f32) void,
-};
+/// Move on the X Axis
+move_x: *const fn (dir: u8, steps: u32) void,
+/// Move on the Y Axis
+move_y: *const fn (dir: u8, steps: u32) void,
+/// Move on the Z Axis
+move_z: *const fn (dir: u8, steps: u32) void,
+/// Move the nozzle extruder
+move_extruder: *const fn (steps: u32) void,
+/// Set the bed's temperature
+set_bed_temp: *const fn (deg_f: f32) void,
+/// Set the temperature of the nozzle
+set_nozzle_temp: *const fn (deg_f: f32) void,
