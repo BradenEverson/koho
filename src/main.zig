27618bde40213f287afdff3c08f9caa75c918a96
BradenEverson <bradeneverson@gmail.com>
2025-07-06 12:35:17 -0500

    Add todo

--- src/main.zig
+++ src/main.zig
@@ -28,12 +28,13 @@ export fn stepper_task(params: ?*anyopaque) callconv(.c) void {
     }
 }
 
+// TODO: I think I need some 10K resistors
 var last_trigger_time: u32 = 0;
 export fn EXTI1_IRQHandler() callconv(.c) void {
     const now = c.HAL_GetTick();
     if (now - last_trigger_time > 5) {
         c.HAL_GPIO_TogglePin(c.LD2_GPIO_Port, c.LD2_Pin);
-        endstop.triggered = true;
+        // endstop.triggered = true;
         last_trigger_time = now;
     }
     c.HAL_GPIO_EXTI_IRQHandler(c.GPIO_PIN_1);
