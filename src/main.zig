91ca9bb8133ae5056cffc9962b2ee5c0da5cad72
BradenEverson <bradeneverson@gmail.com>
2025-07-08 17:39:57 -0500

    Add X axis

--- src/main.zig
+++ src/main.zig
@@ -17,37 +17,73 @@ const Endstop = @import("peripherals/endstop.zig");
 
 const Printer = @import("printer/printer.zig");
 
-var stepper: Stepper = undefined;
-var endstop: Endstop = undefined;
+const pvParameters: ?*anyopaque = null;
+const pxCreatedTask: ?*os.TaskHandle_t = null;
 
-export fn stepper_task(params: ?*anyopaque) callconv(.c) void {
+var x_axis: Stepper = undefined;
+var y_axis: Stepper = undefined;
+
+var x_endstop: Endstop = undefined;
+var y_endstop: Endstop = undefined;
+
+export fn home_x_axis(params: ?*anyopaque) callconv(.c) void {
     _ = params;
 
-    while (!endstop.triggered) {
-        stepper.step();
+    while (!x_endstop.triggered) {
+        x_axis.step();
+    }
+
+    x_axis.swap_dir();
+    for (0..50) |_| {
+        x_axis.step();
     }
 }
 
-// TODO: I think I need some 10K resistors
-var last_trigger_time: u32 = 0;
+export fn home_y_axis(params: ?*anyopaque) callconv(.c) void {
+    _ = params;
+
+    while (!y_endstop.triggered) {
+        y_axis.step();
+    }
+
+    y_axis.swap_dir();
+    for (0..50) |_| {
+        y_axis.step();
+    }
+}
+
+var x_last_trigger_time: u32 = 0;
+export fn EXTI0_IRQHandler() callconv(.c) void {
+    const now = c.HAL_GetTick();
+    if (now - x_last_trigger_time > 5) {
+        x_endstop.triggered = true;
+        x_last_trigger_time = now;
+    }
+    c.HAL_GPIO_EXTI_IRQHandler(c.GPIO_PIN_0);
+}
+
+var y_last_trigger_time: u32 = 0;
 export fn EXTI1_IRQHandler() callconv(.c) void {
     const now = c.HAL_GetTick();
-    if (now - last_trigger_time > 5) {
-        c.HAL_GPIO_TogglePin(c.LD2_GPIO_Port, c.LD2_Pin);
-        endstop.triggered = true;
-        last_trigger_time = now;
+    if (now - y_last_trigger_time > 5) {
+        y_endstop.triggered = true;
+        y_last_trigger_time = now;
     }
     c.HAL_GPIO_EXTI_IRQHandler(c.GPIO_PIN_1);
 }
 
 export fn entry() callconv(.c) void {
-    stepper = Stepper.init(c.GPIOC, c.GPIO_PIN_8, c.GPIOC, c.GPIO_PIN_9);
-    endstop = Endstop.init(c.GPIOB, c.GPIO_PIN_1, c.EXTI1_IRQn);
-    stepper.swap_dir();
+    x_axis = Stepper.init(c.GPIOB, c.GPIO_PIN_8, c.GPIOB, c.GPIO_PIN_9);
+    y_axis = Stepper.init(c.GPIOC, c.GPIO_PIN_8, c.GPIOC, c.GPIO_PIN_9);
 
-    const pvParameters: ?*anyopaque = null;
-    const pxCreatedTask: ?*os.TaskHandle_t = null;
-    _ = os.xTaskCreate(stepper_task, "stepper", 256, pvParameters, 15, pxCreatedTask);
+    x_endstop = Endstop.init(c.GPIOB, c.GPIO_PIN_0, c.EXTI0_IRQn);
+    y_endstop = Endstop.init(c.GPIOB, c.GPIO_PIN_1, c.EXTI1_IRQn);
+
+    y_axis.swap_dir();
+    x_axis.swap_dir();
+
+    _ = os.xTaskCreate(home_x_axis, "home the x axis", 256, pvParameters, 15, pxCreatedTask);
+    _ = os.xTaskCreate(home_y_axis, "home the y axis", 256, pvParameters, 15, pxCreatedTask);
 
     os.vTaskStartScheduler();
     unreachable;
